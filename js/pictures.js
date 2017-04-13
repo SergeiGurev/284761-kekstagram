3b09846671d82572bf432b959b24053bd9bff444
Sergei Gurev <sergeigurev@gmail.com>
2017-04-13 11:04:57 +0300

    Личный проект: подробности (#3)
    * показ/скрытие формы кадрирования и картинки в галерее
    
    * правки по замечаниям
    
    * правки по замечаниям

--- js/pictures.js
+++ js/pictures.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var NUMBER_PHOTOS = 25;
+var KEY_CODE_ENTER = 13;
+var KEY_CODE_ESC = 27;
 
 var commentsAll = [
   'Всё отлично!',
@@ -14,18 +16,22 @@ var commentsAll = [
 var pictures = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture-template').content;
 var galleryOverlay = document.querySelector('.gallery-overlay');
+var galleryOverlayClose = galleryOverlay.querySelector('.gallery-overlay-close');
 var photos = getPhotos(NUMBER_PHOTOS);
+var uploadImageForm = document.getElementById('upload-select-image');
+var uploadImageFormInput = document.getElementById('upload-file');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadForm = uploadOverlay.querySelector('.upload-form');
+var uploadOverlayClose = document.getElementById('upload-cancel');
 
 addPhotos(pictures, photos);
 document.querySelector('.upload-overlay').classList.add('invisible');
-showOverlay(photos[0]);
 
-function showOverlay(photo) {
-  galleryOverlay.classList.remove('invisible');
-  galleryOverlay.querySelector('.gallery-overlay-image').src = photo.url;
-  galleryOverlay.querySelector('.likes-count').textContent = photo.likes;
-  galleryOverlay.querySelector('.comments-count').textContent = photo.comments.length;
-}
+galleryOverlayClose.addEventListener('click', onGalleryOverlayCloseClick);
+galleryOverlayClose.addEventListener('keydown', onGalleryOverlayClosePress);
+uploadImageFormInput.addEventListener('change', onChangeUploadImput);
+uploadOverlayClose.addEventListener('click', onUploadOverlayClosePress);
+uploadForm.addEventListener('submit', onUploadOverlaySubmit);
 
 function addPhotos(element, arrayPhotos) {
   var fragment = document.createDocumentFragment();
@@ -40,7 +46,14 @@ function addPhotos(element, arrayPhotos) {
 
 function createPhotoElement(photo) {
   var photoElement = pictureTemplate.cloneNode(true);
+  var picture = photoElement.querySelector('a');
+  var onPictureClick = function (evt) {
+    evt.preventDefault();
+    openGalleryOverlay(photo);
+  };
 
+  picture.addEventListener('click', onPictureClick);
+  picture.setAttribute('tabindex', '1');
   photoElement.querySelector('img').src = photo.url;
   photoElement.querySelector('.picture-comments').textContent = photo.comments.length;
   photoElement.querySelector('.picture-likes').textContent = photo.likes;
@@ -74,4 +87,60 @@ function getRandomComments() {
   comments.push(commentsAll[Math.floor(Math.random() * commentsAll.length)]);
 
   return comments;
+}
+
+function onPopupEscPress(evt) {
+  if (evt.keyCode === KEY_CODE_ESC) {
+    closeGalleryOverlay();
+  }
+}
+
+function openGalleryOverlay(photo) {
+  galleryOverlay.classList.remove('invisible');
+  galleryOverlay.querySelector('.gallery-overlay-image').src = photo.url;
+  galleryOverlay.querySelector('.likes-count').textContent = photo.likes;
+  galleryOverlay.querySelector('.comments-count').textContent = photo.comments.length;
+  document.addEventListener('keydown', onPopupEscPress);
+}
+
+function closeGalleryOverlay() {
+  galleryOverlay.classList.add('invisible');
+  document.removeEventListener('keydown', onPopupEscPress);
+}
+
+function onGalleryOverlayCloseClick() {
+  closeGalleryOverlay();
+}
+
+function onGalleryOverlayClosePress(evt) {
+  if (evt.keyCode === KEY_CODE_ENTER) {
+    closeGalleryOverlay();
+  }
+}
+
+function onUploadOverlayEscPress(evt) {
+  if (evt.keyCode === KEY_CODE_ESC) {
+    closeUploadOverlay();
+  }
+}
+
+function onChangeUploadImput() {
+  uploadImageForm.classList.add('invisible');
+  uploadOverlay.classList.remove('invisible');
+  document.addEventListener('keydown', onUploadOverlayEscPress);
+}
+
+function closeUploadOverlay() {
+  uploadImageForm.classList.remove('invisible');
+  uploadOverlay.classList.add('invisible');
+  document.removeEventListener('keydown', onUploadOverlayEscPress);
+}
+
+function onUploadOverlaySubmit(evt) {
+  evt.preventDefault();
+  closeUploadOverlay();
+}
+
+function onUploadOverlayClosePress() {
+  closeUploadOverlay();
 }
