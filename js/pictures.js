3b04bd225511692913d5db8c7e6be6d631b4c54d
Sergei Gurev <sergeigurev@gmail.com>
2017-04-20 14:09:14 +0300

    Личный проект: доверяй, но проверяй (#4)
    * взаимодействие с формой + валидация
    
    * правки по замечаниям
    
    * поравил ошибки кодстиля
    
    * еще правки по замечаниям
    
    * кодстиль
    
    * мелкие правки
    
    убрал сброс  полей при отправке формы, тк он уже есть при ее открытии. Добавил очистку поля загрузки при закрытии окна
    
    * еще правки
    
    * немного переделал
    
    * убрал лишнюю проверку

--- js/pictures.js
+++ js/pictures.js
@@ -3,6 +3,10 @@
 var NUMBER_PHOTOS = 25;
 var KEY_CODE_ENTER = 13;
 var KEY_CODE_ESC = 27;
+var SCALE_INPUT_MIN = 25;
+var SCALE_INPUT_MAX = 100;
+var SCALE_INPUT_STEP = 25;
+var DEFAULT_IMAGE_PREVIEW_CLASS = 'filter-image-preview';
 
 var commentsAll = [
   'Всё отлично!',
@@ -23,15 +27,25 @@ var uploadImageFormInput = document.getElementById('upload-file');
 var uploadOverlay = document.querySelector('.upload-overlay');
 var uploadForm = uploadOverlay.querySelector('.upload-form');
 var uploadOverlayClose = document.getElementById('upload-cancel');
+var filtersPanel = uploadOverlay.querySelector('.upload-filter-controls');
+var imagePreview = uploadOverlay.querySelector('.filter-image-preview');
+var incButton = uploadOverlay.querySelector('.upload-resize-controls-button-inc');
+var decButton = uploadOverlay.querySelector('.upload-resize-controls-button-dec');
+var scaleInput = uploadOverlay.querySelector('.upload-resize-controls-value');
+var uploadTextarea = uploadOverlay.querySelector('.upload-form-description');
+var currentFilterClass;
 
 addPhotos(pictures, photos);
 document.querySelector('.upload-overlay').classList.add('invisible');
-
 galleryOverlayClose.addEventListener('click', onGalleryOverlayCloseClick);
 galleryOverlayClose.addEventListener('keydown', onGalleryOverlayClosePress);
-uploadImageFormInput.addEventListener('change', onChangeUploadImput);
+uploadImageFormInput.addEventListener('change', onChangeUploadInput);
 uploadOverlayClose.addEventListener('click', onUploadOverlayClosePress);
 uploadForm.addEventListener('submit', onUploadOverlaySubmit);
+filtersPanel.addEventListener('click', onFilterControlClick);
+incButton.addEventListener('click', onIncButtonPress);
+decButton.addEventListener('click', onDecButtonPress);
+uploadTextarea.addEventListener('invalid', onUploadTextareaInvalid);
 
 function addPhotos(element, arrayPhotos) {
   var fragment = document.createDocumentFragment();
@@ -124,16 +138,27 @@ function onUploadOverlayEscPress(evt) {
   }
 }
 
-function onChangeUploadImput() {
-  uploadImageForm.classList.add('invisible');
-  uploadOverlay.classList.remove('invisible');
-  document.addEventListener('keydown', onUploadOverlayEscPress);
+function onChangeUploadInput() {
+  if (uploadImageFormInput.value !== '') {
+    setDefaultUploadOverlay();
+    uploadImageForm.classList.add('invisible');
+    uploadOverlay.classList.remove('invisible');
+    document.addEventListener('keydown', onUploadOverlayEscPress);
+  }
 }
 
 function closeUploadOverlay() {
   uploadImageForm.classList.remove('invisible');
+  uploadImageFormInput.value = '';
   uploadOverlay.classList.add('invisible');
   document.removeEventListener('keydown', onUploadOverlayEscPress);
+}
+
+function setDefaultUploadOverlay() {
+  imagePreview.className = DEFAULT_IMAGE_PREVIEW_CLASS;
+  setScaleImage(SCALE_INPUT_MAX);
+  uploadTextarea.value = '';
+  uploadTextarea.style.borderColor = '';
 }
 
 function onUploadOverlaySubmit(evt) {
@@ -143,4 +168,51 @@ function onUploadOverlaySubmit(evt) {
 
 function onUploadOverlayClosePress() {
   closeUploadOverlay();
+}
+
+function onUploadTextareaInvalid() {
+  uploadTextarea.style.borderColor = 'red';
+}
+
+function onFilterControlClick(evt) {
+  var target = evt.target;
+
+  if (target === filtersPanel) {
+    return;
+  }
+
+  if (target.value !== 'none') {
+    var newFilterClass = 'filter-' + target.value;
+    imagePreview.classList.add(newFilterClass);
+  }
+
+  imagePreview.classList.remove(currentFilterClass);
+  currentFilterClass = newFilterClass;
+}
+
+function onIncButtonPress() {
+  var scaleValue = parseInt(scaleInput.value, 10);
+
+  if (scaleValue === SCALE_INPUT_MAX) {
+    return;
+  }
+
+  scaleValue += SCALE_INPUT_STEP;
+  setScaleImage(scaleValue);
+}
+
+function onDecButtonPress() {
+  var scaleValue = parseInt(scaleInput.value, 10);
+
+  if (scaleValue === SCALE_INPUT_MIN) {
+    return;
+  }
+
+  scaleValue -= SCALE_INPUT_STEP;
+  setScaleImage(scaleValue);
+}
+
+function setScaleImage(scale) {
+  scaleInput.value = scale + '%';
+  imagePreview.setAttribute('style', 'transform: scale(' + (scale / 100) + ')');
 }
