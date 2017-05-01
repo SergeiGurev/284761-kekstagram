47cae9e8ce60c43b2650950335a7145d1c5f186b
SergeiGurev <SergeiGurev@gmail.com>
2017-05-01 08:14:27 +0300

    правки в модуле фильтров

--- js/initialize-filters.js
+++ js/initialize-filters.js
@@ -1,17 +1,23 @@
 'use strict';
 
 window.initializeFilters = (function () {
-  return function (filtersElement, cb) {
+  var cb = null;
+
+  return function (filtersElement, setFilter) {
+    cb = setFilter;
+
     filtersElement.addEventListener('click', onFilterElementClick);
+  };
 
-    function onFilterElementClick(evt) {
-      var target = evt.target;
+  function onFilterElementClick(evt) {
+    var target = evt.target;
 
-      if (!target.value) {
-        return;
-      }
+    if (!target.value) {
+      return;
+    }
 
+    if (typeof cb === 'function') {
       cb(target.value);
     }
-  };
+  }
 })();
